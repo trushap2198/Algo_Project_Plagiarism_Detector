07e85addfa52bcd242fcb5d1b4b38a8346636694
trushap2198 <trushap2198@gmail.com>
2022-11-21 23:25:53 -0500

    updated lcs

--- src/src/Detector_40192614.java
+++ src/src/Detector_40192614.java
@@ -16,7 +16,7 @@ public class Detector_40192614 {
         String s1;
         String s2;
         for (i = 1; i <= 6; ++i) {
-            System.out.println("file new");
+            System.out.println("file new"+i);
             line = "data/okay0" + i + "/1.txt,data/okay0" + i + "/2.txt";
             fileArr = line.split(",");
             s1 = readFile(fileArr[0]);
@@ -26,13 +26,11 @@ public class Detector_40192614 {
             List<StringBuilder> str2 = preProcess(s2);
             String[] str_array1  = Arrays.deepToString(str1.toArray()).replaceAll("\\[", "").replaceAll("\\]", "").split(",");
             String[] str_array2 = Arrays.deepToString(str2.toArray()).replaceAll("\\[", "").replaceAll("\\]", "").split(",");
-            //System.out.println(str_array1[0]);
-            //System.out.println(str_array2[0]);
             System.out.println(detect_lcs(str_array1,str_array2));
         }
 
         for(i = 1; i <= 7; ++i) {
-            System.out.println("file new in pal");
+            System.out.println("file new in pal"+i);
             line = "data/plagiarism0" + i + "/1.txt,data/plagiarism0" + i + "/2.txt";
             fileArr = line.split(",");
             s1 = readFile(fileArr[0]);
@@ -42,8 +40,6 @@ public class Detector_40192614 {
             List<StringBuilder> str2 = preProcess(s2);
             String[] str_array1  = Arrays.deepToString(str1.toArray()).replaceAll("\\[", "").replaceAll("\\]", "").split(",");
             String[] str_array2 = Arrays.deepToString(str2.toArray()).replaceAll("\\[", "").replaceAll("\\]", "").split(",");
-            //System.out.println(str_array1[0]);
-            //System.out.println(str_array2[0]);
             System.out.println(detect_lcs(str_array1,str_array2));
         }
 
@@ -59,12 +55,13 @@ public class Detector_40192614 {
             StringBuilder token = new StringBuilder(st.nextToken().toLowerCase()); //tolowercase
             HashSet<String> stopWords = new HashSet<>(Arrays.asList("a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while",
                     "who", "whom", "why", "will", "with", "would", "yet", "you", "your"));
-            if (!stopWords.contains(token.toString())) {
-                //System.out.println(token.getClass());//StopWords Removal
-                tokens.add(token);
-            }
+//            if (!stopWords.contains(token.toString())) {
+////                //System.out.println(token.getClass());//StopWords Removal
+//               tokens.add(token);
+//            }
+            tokens.add(token);
         }
-        tokens = Stemmer.stemming(tokens);
+        //tokens = Stemmer.stemming(tokens);
         return tokens;
     }
 
@@ -86,7 +83,11 @@ public class Detector_40192614 {
         }
 
         //System.out.println(str1.length + ": " + str2.length);
-        return (c[str1.length][str2.length]/(Math.max(str1.length, str2.length * 1.00))) * 100;
+
+        System.out.println("file1 length:"+str1.length);
+        System.out.println("file2 length:"+str2.length);
+        System.out.println("count"+c[str1.length][str2.length]);
+        return (1.00 * c[str1.length][str2.length])/Math.min(str1.length, str2.length)*100;
     }
 
 
