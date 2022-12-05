ef7b1fd2735c14c733f9d8410714ca3068693028
trushap2198 <trushap2198@gmail.com>
2022-12-05 07:04:12 -0500

    similarity fixed

--- src/src/Detector_40192614.java
+++ src/src/Detector_40192614.java
@@ -47,7 +47,7 @@ public class Detector_40192614 {
                 String str2 = preProcessTextFiles(s2);
                 plag = detectLCSInTextFiles(str1,str2,false);
                 System.out.println("Plagiarism:" + plag);
-                if(plag>5.0){
+                if(plag>60.0){
                     isplag = 1;
                 }
             }
@@ -60,6 +60,7 @@ public class Detector_40192614 {
 //
         for(i = 1; i <= 9; ++i) {
             long startTime = 0L;
+            isplag=0;
             System.out.println("Folder: plagiarism0"+i);
             line = "data/plagiarism0" + i + "/1.txt,data/plagiarism0" + i + "/2.txt";
             fileArr = line.split(",");
@@ -91,7 +92,7 @@ public class Detector_40192614 {
                 plag = detectLCSInTextFiles(str1,str2,false);
                 System.out.println("Detection done");
                 System.out.println("Plagiarism:" + plag);
-                if(plag>=5.0){
+                if(plag>=60.0){
                     isplag = 1;
                 }
             }
@@ -112,7 +113,7 @@ public class Detector_40192614 {
         double total_cosine = 0.00;
         HashSet<String> stopWords = new HashSet<>(Arrays.asList("a", "able", "about", "across", "after", "all", "almost", "also", "am", "an",
                 "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either",
-                "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "i",
+                "else", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "i",
                 "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither",
                 "no", "nor", "not", "of", "off", "often", "one","on", "only", "or", "other", "own", "rather", "said", "say", "says", "she", "should",
                 "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas",
@@ -178,7 +179,7 @@ public class Detector_40192614 {
 //        }
 //        //System.out.println("Size of unique corpus:" + uniqueTerms.size());
 //        System.out.println("Total Cosine:" + total_cosine);
-        return total_cosine*100;
+        return cosine_simialr*100;
 
 
     }
