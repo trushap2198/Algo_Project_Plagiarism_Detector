b93bf0b78edc982f20d3d79bb30dc582f3651112
trushap2198 <trushap2198@gmail.com>
2022-12-05 06:59:08 -0500

    cosine similarity added

--- src/src/Detector_40192614.java
+++ src/src/Detector_40192614.java
@@ -1,5 +1,4 @@
-import java.io.File;
-import java.io.RandomAccessFile;
+import java.io.*;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -7,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class Detector_40192614 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         String line;
         int i;
@@ -17,54 +16,58 @@ public class Detector_40192614 {
         String s2;
         double plag = 0;
 
-//        for (i = 1; i <= 6; ++i) {
-//            final long startTime = System.currentTimeMillis();
-//            System.out.println("Folder:okay0"+i);
-//            line = "data/okay0" + i + "/1.txt,data/okay0" + i + "/2.txt";
-//            fileArr = line.split(",");
-//            s1 = readFile(fileArr[0]);
-//            s2 = readFile(fileArr[1]);
-//            if(isCodeFile(s1) && isCodeFile(s2)){
-//                List<StringBuilder> str1 = preProcessCodeFiles(s1);
-//                List<StringBuilder> str2 = preProcessCodeFiles(s2);
-//                String[] str_array1  = Arrays.deepToString(str1.toArray()).replaceAll("\\[", "").replaceAll("\\]", "").split(",");
-//                String[] str_array2 = Arrays.deepToString(str2.toArray()).replaceAll("\\[", "").replaceAll("\\]", "").split(",");
-//                plag = detect_lcs(str_array1,str_array2,true);
-//                if(plag>=55.00){
-//                    isplag = 1;
-//                }
+        for (i = 1; i <= 7; ++i) {
+            isplag = 0;
+            final long startTime = System.currentTimeMillis();
+            System.out.println("Folder:okay0"+i);
+            line = "data/okay0" + i + "/1.txt,data/okay0" + i + "/2.txt";
+            fileArr = line.split(",");
+            s1 = readFile(fileArr[0]);
+            s2 = readFile(fileArr[1]);
+            boolean file1 = isCodeFile(fileArr[0]);
+            boolean file2 = isCodeFile(fileArr[1]);
+            if(file1 && file2){
+                List<StringBuilder> str1 = preProcessCodeFiles(s1);
+                List<StringBuilder> str2 = preProcessCodeFiles(s2);
+                String[] str_array1  = Arrays.deepToString(str1.toArray()).replaceAll("\\[", "").replaceAll("\\]", "").split(",");
+                String[] str_array2 = Arrays.deepToString(str2.toArray()).replaceAll("\\[", "").replaceAll("\\]", "").split(",");
+                plag = detect_lcs(str_array1,str_array2,true);
+                if(plag>=55.00){
+                    isplag = 1;
+                }
+
+            }
+
+            else if (file2 || file1) {
+                System.out.println("One is code file other is not. Not Plagiarised");
+                isplag = 0;
+            }
+            else{
+                String str1 = preProcessTextFiles(s1);
+                String str2 = preProcessTextFiles(s2);
+                plag = detectLCSInTextFiles(str1,str2,false);
+                System.out.println("Plagiarism:" + plag);
+                if(plag>5.0){
+                    isplag = 1;
+                }
+            }
+            System.out.println( "Plagiarism percentage is:" + plag +". Hence the plag score is:"+ isplag + " \n\n");
+
+            final long endTime = System.currentTimeMillis();
+            System.out.println("Total execution time: " + (endTime - startTime) + " \n\n");
+
+        }
 //
-//            }
-//
-//            else if (isCodeFile(s1) || isCodeFile(s2)) {
-//                System.out.println("One is code file other is not. Not Plagiarised");
-//                isplag = 0;
-//            }
-//            else{
-//                List<String> str1 = preProcessTextFiles(s1);
-//                List<String> str2 = preProcessTextFiles(s2);
-//                plag = detectLCSInTextFiles(str1,str2,false);
-//                System.out.println("Plagiarism:" + plag);
-//                if(plag>5.0){
-//                    isplag = 1;
-//                }
-//            }
-//            System.out.println( "Plagiarism percentage is:" + plag +". Hence the plag score is:"+ isplag + " \n\n");
-//
-//            final long endTime = System.currentTimeMillis();
-//            System.out.println("Total execution time: " + (endTime - startTime) + " \n\n");
-//
-//        }
-//
-        for(i = 8; i <= 9; ++i) {
+        for(i = 1; i <= 9; ++i) {
             long startTime = 0L;
             System.out.println("Folder: plagiarism0"+i);
             line = "data/plagiarism0" + i + "/1.txt,data/plagiarism0" + i + "/2.txt";
             fileArr = line.split(",");
             s1 = readFile(fileArr[0]);
             s2 = readFile(fileArr[1]);
-            if(isCodeFile(s1) && isCodeFile(s2)){
-                System.out.println("here in code");
+            boolean file1 = isCodeFile(fileArr[0]);
+            boolean file2 = isCodeFile(fileArr[1]);
+            if(file1 && file2){
                 startTime = System.currentTimeMillis();
                 List<StringBuilder> str1 = preProcessCodeFiles(s1);
                 List<StringBuilder> str2 = preProcessCodeFiles(s2);
@@ -77,14 +80,14 @@ public class Detector_40192614 {
                 
             }
 
-            else if (isCodeFile(s1) || isCodeFile(s2)){
+            else if (file2 || file1){
                 System.out.println("One is code file other is not. Not Plagiarised");
                 isplag = 0;
             }
             else{
                 startTime = System.currentTimeMillis();
-                List<String> str1 = preProcessTextFiles(s1);
-                List<String> str2 = preProcessTextFiles(s2);
+                String str1 = preProcessTextFiles(s1);
+                String str2 = preProcessTextFiles(s2);
                 plag = detectLCSInTextFiles(str1,str2,false);
                 System.out.println("Detection done");
                 System.out.println("Plagiarism:" + plag);
@@ -99,51 +102,105 @@ public class Detector_40192614 {
 
     }
 
-    private static double detectLCSInTextFiles(List<String> str1, List<String> str2, boolean iscodeFile) {
+    private static double detectLCSInTextFiles(String str1, String str2, boolean iscodeFile) {
 
-        System.out.println("In detection");
-        List<String> string1;
-        List<String> string2;
-
-        double match, max_match_in_one_sentence,total_match = 0.00;
-        int word_size= 0;
-        if(str1.size()>str2.size())
-        {
-            string1 = str2;
-            string2 = str1;
-        }
-        else {
-            string1 = str1;
-            string2 = str2;
-        }
-        for (String sent1:string1){
-            word_size+=sent1.length();
-        }
-        for(String sent1:string1){
-            max_match_in_one_sentence = 0.00;
-            for(String sent2:string2){
-                match = detect_lcs(sent1.split(" "),sent2.split(" "),iscodeFile);
-                if(max_match_in_one_sentence<match){
-                    max_match_in_one_sentence= match;
+        HashSet<String> uniqueTerms = new HashSet<>();
+        Map<String, Double> weights1 = new HashMap<>();
+        Map<String, Double> weights2 = new HashMap<>();
+        String[] words1 = str1.toLowerCase().split("\\s+");
+        String[] words2 = str2.toLowerCase().split("\\s+");
+        double total_cosine = 0.00;
+        HashSet<String> stopWords = new HashSet<>(Arrays.asList("a", "able", "about", "across", "after", "all", "almost", "also", "am", "an",
+                "and", "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either",
+                "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "i",
+                "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither",
+                "no", "nor", "not", "of", "off", "often", "one","on", "only", "or", "other", "own", "rather", "said", "say", "says", "she", "should",
+                "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas",
+                "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while",
+                "who", "whom", "why", "will", "with", "would", "yet", "you", "your"));
+        for(String word:words1){
+            if(!stopWords.contains(word)) {
+                if (!weights1.containsKey(word)) {
+                    weights1.put(word, 0.0);
                 }
-            }
-            total_match+=max_match_in_one_sentence;
-            //the breakout
-            if((total_match/word_size*100) > 5.0){
-                System.out.println("Breakout");
-                return total_match/word_size*100;
+                weights1.put(word, weights1.get(word) + 1.0);
+            }    uniqueTerms.add(word);
+        }
+        for(String word:words2){
+            if(!stopWords.contains(word)) {
+                if (!weights2.containsKey(word)) {
+                    weights2.put(word, 0.0);
+                }
+                weights2.put(word, weights2.get(word) + 1.0);
+                uniqueTerms.add(word);
             }
         }
-        System.out.println("Exit detect");
-        return total_match/word_size*100;
+        double cosine_simialr = detect_lcs_cosine(weights1,weights2,uniqueTerms);
+        System.out.println("Cosine similarity:" + (cosine_simialr));
+
+
+//        for(String sent1: str1){
+//            uniqueTerms.clear();
+//            double max_similar = 0.0;
+//            String[] words = sent1.split(" ");
+//            for(String word: words){
+//                word = word.trim();
+//                if(!word.isEmpty()){
+//                    if(!weights1.containsKey(word)){
+//                        weights1.put(word,0.0);
+//                    }
+//                    weights1.put(word,weights1.get(word)+1.0);
+//                    uniqueTerms.add(word);
+//                }
+//            }
+//            for (String sent2: str2){
+//                String[] words2 = sent2.split(" ");
+//                for(String word: words2){
+//                    word = word.trim();
+//                    if(!word.isEmpty()){
+//                        if(!weights2.containsKey(word)){
+//                            weights2.put(word,0.0);
+//                        }
+//                        weights2.put(word,weights2.get(word)+1.0);
+//                        //uniqueTerms.add(word);
+//                    }
+//                }
+//
+//                double current_cosine = detect_lcs_cosine(weights1,weights2,uniqueTerms);
+//                if(max_similar<current_cosine){
+//                    max_similar = current_cosine;
+//                }
+//            }
+//            total_cosine+=max_similar/ uniqueTerms.size();
+////            if(total_cosine/uniqueTerms.size()>10.0){
+////                return 10.01;
+////            }
+//        }
+//        //System.out.println("Size of unique corpus:" + uniqueTerms.size());
+//        System.out.println("Total Cosine:" + total_cosine);
+        return total_cosine*100;
+
+
     }
 
-    private static List<String> preProcessTextFiles(String s2) {
-        System.out.println("in prepreprocess");
+    private static double detect_lcs_cosine(Map<String, Double> weights1, Map<String, Double> weights2, HashSet<String> uniqueTerms) {
+        double dot1=0.0,dot2 = 0.0,dot_final=0.0;
+        for (String term:uniqueTerms){
+            double count1 = weights1.getOrDefault(term,0.0);
+            double count2 = weights2.getOrDefault(term,0.0);
+            dot1 += (count1*count1);
+            dot2+=(count2*count2);
+            dot_final+= (count1*count2);
 
-        String str = s2.replaceAll("[,;:'!?$%\\^*]","");
-        List<String> sentences = new ArrayList<>();
-        List<String> paras = Arrays.stream(str.split("\\n")).collect(Collectors.toList());
+        }
+        return (dot_final/ (Math.sqrt(dot1) * Math.sqrt(dot2)));
+    }
+
+
+    private static String preProcessTextFiles(String s2) {
+        String str = s2.replaceAll("[^a-zA-Z ]", "");
+        StringBuilder sentences = new StringBuilder();
+        String[] paras = str.split("\\n");
         List<String> ref_keywords = Arrays.asList("Wikipedia", "http","www", ".org","wiki","wikipedia","#","google",".com","https");
         int ref_count=0;
         Pattern matching_date = Pattern.compile("[0-9]{4}");
@@ -160,13 +217,14 @@ public class Detector_40192614 {
                     ref_count++;
                 }
             }
+
             if((100.00 * ref_count/para.length())<=0.90){
-                //System.out.println("Not a reference");
-                sentences.addAll(Arrays.stream(para.split("\\.")).collect(Collectors.toList()));
+                System.out.println("Not a reference");
+                sentences.append(para);
             }
         }
-        System.out.println("Done preprocess");
-        return sentences;
+
+        return sentences.toString();
     }
 
     private static List<StringBuilder> preProcessCodeFiles(String string) {
@@ -202,36 +260,7 @@ public class Detector_40192614 {
                 }
             }
         }
-//
-//        System.out.println("Printing the longest subsequence:");
-//        int sequ_size = c[str1.length][str2.length];
-//        int temp = sequ_size;
-//        String[] lcs = new String[  sequ_size+  1];
-//        lcs[sequ_size]  = "\0";
-//
-//        int i = str1.length, j = str2.length;
-//        while (i > 0 && j > 0) {
-//            if (str1[i - 1].equals(str2[j - 1])) {
-//                lcs[sequ_size - 1] = str1[i - 1];
-//                i--;
-//                j--;
-//                sequ_size--;
-//            }
-//
-//            else if (c[i - 1][j] > c[i][j - 1])
-//                i--;
-//            else
-//                j--;
-//        }
-//
-//        System.out.print("\nLCS: ");
-//        for (int k = 0; k <= temp; k++)
-//            System.out.print(lcs[k]);
-//        System.out.println("");
 
-//        System.out.println("file1 length:"+str1.length);
-//        System.out.println("file2 length:"+str2.length);
-//        System.out.println("count: "+c[str1.length][str2.length]);
         if(isCode)
         return (1.00 * c[str1.length][str2.length])/Math.min(str1.length, str2.length)*100;
         else
@@ -273,37 +302,37 @@ public class Detector_40192614 {
 
 
     private static String readFile(String fileName) {
-        String contents = "";
+        StringBuilder contents = new StringBuilder();
         try {
-            RandomAccessFile fin = new RandomAccessFile(new File(fileName), "r");
-
-            for (int b = fin.read(); b != -1; b = fin.read()) {
-                //System.out.println("Okay");
-                contents = contents + (char) b;
+            BufferedReader fin = new BufferedReader(new FileReader(fileName));
+            String line = "";
+            while((line = fin.readLine())!= null){
+                contents.append(line+"\n");
             }
         } catch (Exception var4) {
             System.err.println("Trouble reading from: " + fileName);
         }
-        //System.out.println("CON"+ contents);
-        return contents;
+        return contents.toString();
     }
 
-    private static boolean isCodeFile(String text) {
-        ArrayList<String> keywords = new ArrayList<>(Arrays.asList("#include", "elif", "class", "void", "bool", "h>", "int", "char", "chr", "return", "char", "cout", "<<", ">>", "==", "!=", ">=", "<=", "{", "}", "=", "++", "--", "<", ">", "++", "--", "switch", "case", "()", "system", "out", "println", "print", "boolean", "static", "void"));
-        double count = 0.00;
-        for (String str : text.split(" ")) {
-            if (keywords.contains(str)) {
-                count++;
-            }
-            if (count / text.length() >= 0.01) {
+    private static boolean isCodeFile(String text) throws FileNotFoundException {
+        int count_processed = 0;
+        Scanner fin = new Scanner(new FileReader(text));
+        String line = "";
+        while(fin.hasNextLine())
+        {
+            line = fin.nextLine().trim();
+            if (line.startsWith("#include") || line.startsWith("int ")||line.startsWith("chr ")||line.startsWith("char ")||line.startsWith("/** ")||line.startsWith("/* ") ||
+            line.startsWith("bool ") || line.startsWith("boolean ")|| line.startsWith("public static void") || line.startsWith("System.out.println"))
+            {
                 return true;
             }
+            count_processed++;
+            if (count_processed==200){
+                return false;
+            }
         }
-        //System.out.println("This file has "+ count + " codewords out of " + text.length() + " words");
-        if (count / text.length() >= 0.01) {
-            return true;
-        } else
-            return false;
+        return false;
     }
 
 }
